24be20ffc4d406d433eec7681e297bb2b2247c4a
oscarOT09 <135353255+oscarOT09@users.noreply.github.com>
2025-07-11 17:39:03 -0600

    Improved workflow with user input checks

--- main.cpp
+++ main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <algorithm>
 #include "Circle.h"
 #include "Triangle.h"
 #include "Pentagon.h"
 #include "Logger.h"
 using namespace std;
 
+double readAndCheckDoubleInput(const string& message) {
+    double value;
+    Logger::log(message);
+    cin >> value;
+    while (cin.fail() || value <= 0) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        Logger::log("Invalid input. Introduce a positive integer:");
+        cin >> value;
+    }
+    return value;
+}
+
+string readAndCheckStringInput(const string& message){
+    string color;
+    Logger::log(message);
+    cin >> color;
+    while (color.empty() || !all_of(color.begin(), color.end(), ::isalpha)) {
+       Logger::log("Invalid color. Introduce a color without numbers and special characters:");
+       cin >> color;
+    }
+    return color;
+}
+
 int main(){
     Logger::initTime();
-    
+
     double circle_radius;
     double triangle_heigth,
            triangle_s1Len,
@@ -22,43 +47,30 @@ int main(){
            pentagon_color;
 
     ///// PARAMETERS REQUEST /////
-    // Circle parameters
     Logger::log("Introduce Circle object parameters");
-    Logger::log("Radius:");
-    cin >> circle_radius;
+    circle_radius = readAndCheckDoubleInput("Radius:"); 
     
-    // Triangle parameters
     Logger::log("Introduce Triangle object parameters");
-    Logger::log("Height:");
-    cin >> triangle_heigth;
-    Logger::log("Side 1 length:");
-    cin >> triangle_s1Len;
-    Logger::log("Side 2 length:");
-    cin >> triangle_s2Len;
-    Logger::log("Base (side 3) length:");
-    cin >> triangle_baseLen;    
+    triangle_heigth = readAndCheckDoubleInput("Height:");
+    triangle_s1Len = readAndCheckDoubleInput("Side 1 length:");
+    triangle_s2Len = readAndCheckDoubleInput("Side 2 length:");
+    triangle_baseLen = readAndCheckDoubleInput("Base (side 3) length:");
 
-    // Pentagon parameters
     Logger::log("Introduce Pentagon object parameters");
-    Logger::log("Sides length:");
-    cin >> pentagon_sidesLen;
-    Logger::log("Apothem:");
-    cin >> pentagon_apothem;
+    pentagon_sidesLen = readAndCheckDoubleInput("Sides length:");
+    pentagon_apothem = readAndCheckDoubleInput("Apothem:");
+    
+    Logger::log("");
 
     ///// OBJECTS CONSTRUCTION /////
     unique_ptr<Circle> circle1 = make_unique<Circle>(circle_radius);
     unique_ptr<Triangle> triangle1 = make_unique<Triangle>(triangle_heigth, triangle_s1Len, triangle_s2Len, triangle_baseLen);
     unique_ptr<Pentagon> pentagon1 = make_unique<Pentagon>(pentagon_sidesLen, pentagon_apothem);
-    
-    Logger::log("");
 
     ///// COLORS REQUEST /////
-    Logger::log("Introduce Circle object color");
-    cin >> circle_color;
-    Logger::log("Introduce Triangle object color");
-    cin >> triangle_color;
-    Logger::log("Introduce Pentagon object color");
-    cin >> pentagon_color;
+    circle_color = readAndCheckStringInput("Introduce Circle object color:");
+    triangle_color = readAndCheckStringInput("Introduce Triangle object color:");
+    pentagon_color = readAndCheckStringInput("Introduce Pentagon object color:");
 
     Logger::log("");
 
@@ -67,32 +79,23 @@ int main(){
     triangle1->setColor(triangle_color);
     pentagon1->setColor(pentagon_color);
 
-    ///// PRINT AREAS ///// 
-    Logger::log("AREAS");
-    Logger::log("circle1:");
+    ///// PRINT RESULTS ///// 
+    Logger::log("RESULTS");
+    Logger::log("Circle object");
     Logger::logArea(to_string(circle1->getArea()));
-    Logger::log("triangle1:");
-    Logger::logArea(to_string(triangle1->getArea()));
-    Logger::log("pentagon1:");
-    Logger::logArea(to_string(pentagon1->getArea()));
-
+    Logger::logPerimeter(to_string(circle1->getPerimeter()));
+    circle1->printMembers();
     Logger::log("");
 
-    ///// PRINT PERIMETERS /////
-    Logger::log("PERIMETERS");
-    Logger::log("circle1:");
-    Logger::logPerimeter(to_string(circle1->getPerimeter()));
-    Logger::log("triangle1:");
+    Logger::log("Triangle object");
+    Logger::logArea(to_string(triangle1->getArea()));
     Logger::logPerimeter(to_string(triangle1->getPerimeter()));
-    Logger::log("pentagon1:");
-    Logger::logPerimeter(to_string(pentagon1->getPerimeter()));
-
+    triangle1->printMembers();
     Logger::log("");
     
-    ///// PRINT MEMBERS /////
-    Logger::log("OBJECTS MEMBERS");
-    circle1->printMembers();   
-    triangle1->printMembers();
+    Logger::log("Pentagon object");
+    Logger::logArea(to_string(pentagon1->getArea()));
+    Logger::logPerimeter(to_string(pentagon1->getPerimeter()));
     pentagon1->printMembers();
 
     return 0;
